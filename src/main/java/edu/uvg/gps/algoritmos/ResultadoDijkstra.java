59cc545eef827f65b1a01f0b84037e0e07056946
Bryant "Jason <111195815+NET-Jason@users.noreply.github.com>
2026-05-29 17:00:09 -0600

    Estructura de las rutas

--- src/main/java/edu/uvg/gps/algoritmos/ResultadoDijkstra.java
+++ src/main/java/edu/uvg/gps/algoritmos/ResultadoDijkstra.java
@@ -5,16 +5,27 @@ public class ResultadoDijkstra {
     private int totalPasos;
     private double costoTotal;
     private boolean esTiempo;
+    private PeriodoHorario periodo;
 
     public ResultadoDijkstra(String[] camino, int totalPasos, double costoTotal, boolean esTiempo) {
         this.camino = camino;
         this.totalPasos = totalPasos;
         this.costoTotal = costoTotal;
         this.esTiempo = esTiempo;
+        this.periodo = null;
+    }
+
+    public ResultadoDijkstra(String[] camino, int totalPasos, double costoTotal, PeriodoHorario periodo) {
+        this.camino = camino;
+        this.totalPasos = totalPasos;
+        this.costoTotal = costoTotal;
+        this.esTiempo = true;
+        this.periodo = periodo;
     }
 
     public String[] getCamino() { return camino; }
     public int getTotalPasos() { return totalPasos; }
     public double getCostoTotal() { return costoTotal; }
     public boolean isEsTiempo() { return esTiempo; }
+    public PeriodoHorario getPeriodo() { return periodo; }
 }
--- src/main/java/edu/uvg/gps/model/NodoAdyacencia.java
+++ src/main/java/edu/uvg/gps/model/NodoAdyacencia.java
@@ -3,14 +3,28 @@ package edu.uvg.gps.model;
 public class NodoAdyacencia {
     private String ciudadDestino;
     private double distancia;
-    private double tiempo;
+    private double tiempoBase;
+    private double[] multiplicadores;
     private NodoAdyacencia siguiente;
 
     public NodoAdyacencia(String ciudadDestino, double distancia, double tiempo) {
         this.ciudadDestino = ciudadDestino;
         this.distancia = distancia;
-        this.tiempo = tiempo;
+        this.tiempoBase = tiempo;
+        this.multiplicadores = new double[]{1.0, 1.0, 1.0, 1.0, 1.0};
         this.siguiente = null;
+    }
+
+    public NodoAdyacencia(String ciudadDestino, double distancia, double tiempoBase, double[] multiplicadores) {
+        this.ciudadDestino = ciudadDestino;
+        this.distancia = distancia;
+        this.tiempoBase = tiempoBase;
+        this.multiplicadores = multiplicadores;
+        this.siguiente = null;
+    }
+
+    public double getTiempoEfectivo(int indicePeriodo) {
+        return tiempoBase * multiplicadores[indicePeriodo];
     }
 
     public String getCiudadDestino() { return ciudadDestino; }
@@ -19,8 +33,11 @@ public class NodoAdyacencia {
     public double getDistancia() { return distancia; }
     public void setDistancia(double distancia) { this.distancia = distancia; }
 
-    public double getTiempo() { return tiempo; }
-    public void setTiempo(double tiempo) { this.tiempo = tiempo; }
+    public double getTiempo() { return tiempoBase; }
+    public void setTiempo(double tiempo) { this.tiempoBase = tiempo; }
+
+    public double getTiempoBase() { return tiempoBase; }
+    public double[] getMultiplicadores() { return multiplicadores; }
 
     public NodoAdyacencia getSiguiente() { return siguiente; }
     public void setSiguiente(NodoAdyacencia siguiente) { this.siguiente = siguiente; }
@@ -29,6 +46,6 @@ public class NodoAdyacencia {
     public String toString() {
         return "-> " + ciudadDestino +
                 " (" + String.format("%.2f", distancia) + " km" +
-                " | " + String.format("%.1f", tiempo) + " min)";
+                " | " + String.format("%.1f", tiempoBase) + " min base)";
     }
 }
